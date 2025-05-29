e3cd78e7a77f2cff1aa9005c15f0972c92ccd28a
David Wang <menghangwang@g.harvard.edu>
2025-05-29 19:36:54 -0400

    Remove customized Rescaling  and will later use updated nequip's PerTypeScaleShift

--- nequip_nac/nn/__init__.py
+++ nequip_nac/nn/__init__.py
@@ -1,5 +1,4 @@
 from ._nac_module import NACProcessor
 from ._grad_output import NACForceOutput
-from ._atomwise import PerTypeScaleShift
 
-__all__ = [NACProcessor, NACForceOutput, PerTypeScaleShift]
+__all__ = [NACProcessor, NACForceOutput]
