4c63d4346e5d5d58707bd4d01891f7a67fc50205
David Wang <menghangwang@g.harvard.edu>
2025-05-29 11:14:12 -0400

    Correct and reformat

--- nequip_nac/_keys.py
+++ nequip_nac/_keys.py
@@ -1,7 +1,6 @@
 from typing import Final, List
 
-from nequip.data import register_fields
-from nequip.train._key import ABBREV
+from nequip.data import register_fields, ABBREV
 
 NAC_KEY: Final[str] = "nac"
 ENERGY_0_KEY: Final[str] = "energy_0"
@@ -13,16 +12,13 @@ PER_ATOM_ENERGY_1_KEY: Final[str] = "per_atom_energy_1"
 
 register_fields(
     graph_fields=[ENERGY_0_KEY, ENERGY_1_KEY],
-    node_fields=[NAC_KEY, FORCE_0_KEY, FORCE_1_KEY, PER_ATOM_ENERGY_0_KEY, PER_ATOM_ENERGY_0_KEY],
+    node_fields=[NAC_KEY, FORCE_0_KEY, FORCE_1_KEY, PER_ATOM_ENERGY_0_KEY, PER_ATOM_ENERGY_1_KEY],
 )
 
 ALL_ENERGY_E0_KEYS: Final[List[str]] = [
     PER_ATOM_ENERGY_0_KEY,
     ENERGY_0_KEY,
     FORCE_0_KEY,
-    # NAC_KEY,            # rescale NAC with energy_0
-    # EDGE_ENERGY_KEY,    # not sure if this is needed for scaling
-    # PARTIAL_FORCE_KEY,
 ]
 
 ALL_ENERGY_E1_KEYS: Final[List[str]] = [
@@ -31,11 +27,12 @@ ALL_ENERGY_E1_KEYS: Final[List[str]] = [
     FORCE_1_KEY,
 ]
 
-ABBREV.update({NAC_KEY: "nac",
-               ENERGY_0_KEY: "e0",
-               ENERGY_1_KEY: "e1",
-               FORCE_0_KEY: "f0",
-               FORCE_1_KEY: "f1",
-               PER_ATOM_ENERGY_0_KEY: "E0i",
-               PER_ATOM_ENERGY_1_KEY: "E1i",
-               })
+ABBREV.update({
+    NAC_KEY: "nac",
+    ENERGY_0_KEY: "e0",
+    ENERGY_1_KEY: "e1",
+    FORCE_0_KEY: "f0",
+    FORCE_1_KEY: "f1",
+    PER_ATOM_ENERGY_0_KEY: "E0pA",
+    PER_ATOM_ENERGY_1_KEY: "E1pA",
+    })
