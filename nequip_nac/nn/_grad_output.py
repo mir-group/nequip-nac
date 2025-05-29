f14f906978dbbcd561fae7c13882fe82d9ba02f8
David Wang <menghangwang@g.harvard.edu>
2025-05-29 11:17:12 -0400

    Change for potential multi-states input

--- nequip_nac/nn/_grad_output.py
+++ nequip_nac/nn/_grad_output.py
@@ -13,66 +13,81 @@ import pdb
 
 @compile_mode("script")
 class NACForceOutput(GraphModuleMixin, torch.nn.Module):
-    r"""Wrap a model and include as an output its gradient.
+    r"""Compute forces from energies of two electronic states.
+
+    This class wraps an energy model that predicts energies for multiple electronic states
+    and computes the corresponding forces using automatic differentiation.
 
     Args:
-        func: the model to wrap
-        of: the name of the output field of ``func`` to take the gradient with respect to. The field must be a single scalar (i.e. have irreps ``0e``)
-        wrt: the input field(s) of ``func`` to take the gradient of ``of`` with regards to.
-        out_field: the field in which to return the computed gradients. Defaults to ``f"d({of})/d({wrt})"`` for each field in ``wrt``.
-        sign: either 1 or -1; the returned gradient is multiplied by this.
-        e.g,
-        of=AtomicDataDict.TOTAL_ENERGY_KEY,
-        wrt=AtomicDataDict.POSITIONS_KEY,
-        out_field=AtomicDataDict.FORCE_KEY,
+        func: the energy model to wrap
+        num_states: number of electronic states (default: 2)
+        energy_key_pattern: pattern for energy field names, formatted with state index
+        force_key_pattern: pattern for force field names, formatted with state index
+
     """
 
     def __init__(
         self,
         func: GraphModuleMixin,
+        num_states: int = 2,
+        energy_key_pattern: str = "energy_{}",
+        force_key_pattern: str = "force_{}",
     ):
         super().__init__()
-
-        # check and init irreps
-        self._init_irreps(
-            irreps_in=func.irreps_in,
-            irreps_out=func.irreps_out,
-        )
         self.func = func
 
-        # define irreps of energy and force
-        self.irreps_out.update(
-            {_keys.FORCE_0_KEY: '1x1o', _keys.FORCE_1_KEY: '1x1o'}
+        self.num_states = num_states
+        self.energy_keys = [energy_key_pattern.format(i) for i in range(num_states)]
+        self.force_keys = [force_key_pattern.format(i) for i in range(num_states)]
+
+        # Validate that all energy fields are scalars
+        energy_irreps_check = {energy_key: Irreps("0e") for energy_key in self.energy_keys}
+
+        # Check and init irreps
+        self._init_irreps(
+            irreps_in=self.func.irreps_in.copy(),
+            my_irreps_in=energy_irreps_check,
+            irreps_out=self.func.irreps_out.copy(),
         )
+
+        # Define irreps of forces for all states
+        force_irreps = {force_key: "1x1o" for force_key in self.force_keys}
+        self.irreps_out.update(force_irreps)
         
 
     def forward(self, data: AtomicDataDict.Type) -> AtomicDataDict.Type:
-        
-        # set req grad
+
+        # Set requires grad for positions
         data[AtomicDataDict.POSITIONS_KEY].requires_grad_(True)
         pos_tensors = data[AtomicDataDict.POSITIONS_KEY]
         
-        # run func
+        # Run the energy model
         data = self.func(data)
-        # Get grads
-        force_0_grads = torch.autograd.grad(
-            [data[_keys.ENERGY_0_KEY].sum()],
-            [pos_tensors],
-            create_graph=self.training,
-            retain_graph=True,  # needed to allow gradients of this output during training
-        )[0]
 
-        data[_keys.FORCE_0_KEY] = torch.neg(force_0_grads)
+        # Calculate forces for all states
+        for i in range(self.num_states):
+            # Retain graph for next state
+            if i < len(self.energy_keys) - 1:
+                retain_graph = True
+            else:
+                retain_graph = self.training
 
-        force_1_grads = torch.autograd.grad(
-            [data[_keys.ENERGY_1_KEY].sum()],
-            [pos_tensors],
-            create_graph=self.training,
-        )[0]
+            energy_key = self.energy_keys[i]
+            force_key = self.force_keys[i]
+            
+            force_grads = torch.autograd.grad(
+                outputs=[data[energy_key].sum()],
+                inputs=[pos_tensors],
+                create_graph=self.training,
+                retain_graph=retain_graph,
+            )[0]
+            
+            if force_grads is None:
+                raise RuntimeError(f"Failed to compute gradient for state {i}")
+            
+            data[force_key] = torch.neg(force_grads)
 
-        data[_keys.FORCE_1_KEY] = torch.neg(force_1_grads)
-
-        # unset requires_grad_
+        # Unset requires_grad
         data[AtomicDataDict.POSITIONS_KEY].requires_grad_(False)
 
         return data
