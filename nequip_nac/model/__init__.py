3d84f36a135912cd8922d867ac28bdb7cd90aa58
David Wang <menghangwang@g.harvard.edu>
2025-05-29 11:34:16 -0400

    Remove modules in _grad

--- nequip_nac/model/__init__.py
+++ nequip_nac/model/__init__.py
@@ -1,4 +1,3 @@
 from ._nequip_nac import NequIPNACModel, FullNequIPNACEnergyModel
-from ._grads import NACForceOutput
 
-__all__ = [NequIPNACModel, FullNequIPNACEnergyModel, NACForceOutput]
+__all__ = [NequIPNACModel, FullNequIPNACEnergyModel]
