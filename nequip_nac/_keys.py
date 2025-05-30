d17a6d4ee1dbff1b5ef7734ab10ab3bdb07cf32b
cw-tan <chuinwei.tan@gmail.com>
2025-05-29 21:52:35 -0400

    clean keys

--- nequip_nac/_keys.py
+++ nequip_nac/_keys.py
@@ -12,7 +12,13 @@ PER_ATOM_ENERGY_1_KEY: Final[str] = "per_atom_energy_1"
 
 register_fields(
     graph_fields=[ENERGY_0_KEY, ENERGY_1_KEY],
-    node_fields=[NAC_KEY, FORCE_0_KEY, FORCE_1_KEY, PER_ATOM_ENERGY_0_KEY, PER_ATOM_ENERGY_1_KEY],
+    node_fields=[
+        NAC_KEY,
+        FORCE_0_KEY,
+        FORCE_1_KEY,
+        PER_ATOM_ENERGY_0_KEY,
+        PER_ATOM_ENERGY_1_KEY,
+    ],
 )
 
 ALL_ENERGY_E0_KEYS: Final[List[str]] = [
@@ -27,12 +33,14 @@ ALL_ENERGY_E1_KEYS: Final[List[str]] = [
     FORCE_1_KEY,
 ]
 
-ABBREV.update({
-    NAC_KEY: "nac",
-    ENERGY_0_KEY: "e0",
-    ENERGY_1_KEY: "e1",
-    FORCE_0_KEY: "f0",
-    FORCE_1_KEY: "f1",
-    PER_ATOM_ENERGY_0_KEY: "E0pA",
-    PER_ATOM_ENERGY_1_KEY: "E1pA",
-    })
+ABBREV.update(
+    {
+        NAC_KEY: "nac",
+        ENERGY_0_KEY: "e0",
+        ENERGY_1_KEY: "e1",
+        FORCE_0_KEY: "f0",
+        FORCE_1_KEY: "f1",
+        PER_ATOM_ENERGY_0_KEY: "E0pA",
+        PER_ATOM_ENERGY_1_KEY: "E1pA",
+    }
+)
